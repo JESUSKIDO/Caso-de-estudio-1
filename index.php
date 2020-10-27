0566171b308c3a5a01a4dad35eae22ce43e7465e
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-27 00:19:01 -0600

    Cambiando archivos
    Se crear la interfaz de la pagina web

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookShop</title>
+</head>
+<body>
+    <link rel="stylesheet" href="CSS/styles2.css">
+    <div class="header">
+        <h1>Librería</h1>
+        <a href="ingresar.php">Ingresar</a>
+        <a  href="libros.php">Libros</a>
+        <a  href="carrito.php">Carrito</a>
+    </div>
+
+
+</body>
+</html>
