c80a5f28fc7d3a27c7ec1514e9ed2ead0de054b3
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-27 00:19:01 -0600

    Nuevo
    Se crear la interfaz de la pagina web

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookShop</title>
+</head>
+<body>
+    <link rel="stylesheet" href="CSS/styles2.css">
+    <div class="header">
+        <h1>Librería</h1>
+        <a href="ingresar.php">Ingresar</a>
+        <a  href="libros.php">Libros</a>
+        <a  href="carrito.php">Carrito</a>
+    </div>
+    <div class="introduccion">
+        <h2>Para ti:</h2>
+        <p>Encuentra los mejores Titulos de libros para </p>
+        <p>las mejores ocaciones, imagina todo un mundo </p>
+        <p>de aventuras a solo unos clicks de distancia</p>
+        <p>compra los libros de tu agrado y se parte de </p>
+        <p>toda nuestra comunidad.</p>
+        <p>Saluditos</p>
+    </div>
+
+</body>
+</html>
