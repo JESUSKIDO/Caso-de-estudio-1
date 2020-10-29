96201930a933f93b060f93fe83abeaaf15634822
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-28 21:39:31 -0600

    Carrito
    creacion de interfaz de carrito

--- carrito.php
+++ carrito.php
@@ -6,6 +6,72 @@
     <title>Document</title>
 </head>
 <body>
+<link rel="stylesheet" href="CSS/styles.css">
+<div class="header">
+    <div style="text-align: center;">
+        <h1 class="title">CARRITO</h1>
+    </div>
+    <div style="text-align: center;background-color: rgb(180, 238, 228);"> 
+    <h1 style="font-size: 20px;">Productos</h1>
+    </div>
     
+    <div style="text-align: center;">
+        <table> 
+            <tr>
+                    <th>id_Producto</th>
+                    <th>Nombre del Libro</th>
+                    <th>Costo</th>
+                    <th>Cantidad</th>
+                    <th>Total</th>
+                    </tr>
+                    <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    
+            </tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    </tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    </tr>
+        </table>
+
+     </div>    
+     <div style="text-align: center;">
+     <input type="button" value="PAGAR"  style="width:300px;height:80px;font-size: 30px;">
+        <img src="IMG/carrito.png"style="width:100px;height:100px;">
+    </div>         
+    
+</div>
 </body>
 </html>
