88aecd5a08be45ce8af2fc3c6c6e675dbfe789d1
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-28 21:37:33 -0600

    Ranking de libros
    Creacion de interfaz de Ranking de libros

---
+++ toplibros.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<link rel="stylesheet" href="CSS/styles.css">
+<div class="header">
+    <div style="text-align: center;">
+        <h1 class="title">TOP LIBROS MAS VENDIDOS</h1>
+    </div>
+    <div style="text-align: center;background-color: gray;">
+            <h1 style="text-aling:center;">Ranking de los Libros</h1>
+     </div>
+     <divs tyle="text-align: center;">
+        <table> 
+            <tr>
+                    <th>Posicion</th>
+                    <th>Nombre del Libro</th>
+                    <th>Nombre del Autor</th>
+                    <th>Copias vendidas</th>
+                    </tr>
+                    <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    
+            </tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+        </table>
+
+     </div>               
+</body>
+</html>
