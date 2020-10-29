42c149a6a1c380802af602cd1b36d4d39300f4a3
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-28 21:40:51 -0600

    historial de compra
    Creacion de Interfaz de historial de compra

---
+++ historialdecompra.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<link rel="stylesheet" href="CSS/styles.css">
+<div class="header">
+    <div style="text-align: center;">
+        <h1 class="title">HISTORIAL DE COMPRAS</h1>
+    </div>
+    <div style="text-align: center;background-color: gray;">
+            <h1 style="text-aling:center;">Compras Realizadas</h1>
+     </div>
+     <div style="text-align: center;">
+        <table> 
+                <tr>
+                    <th>Id de Compra</th>
+                    <th>Nombres de los Libros</th>
+                    <th>fecha de Entrega</th>
+                    <th>Costo Total</th>
+                </tr>
+                <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                </tr>
+                <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                 <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                </tr>
+                <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                </tr>
+                <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                </tr>
+                <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                </tr>
+                <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                </tr>
+        </table>
+     </div>               
+</body>
+</html>
