5547cca8b676b610fccc5b8cf27f5f9e2cea860d
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-28 21:41:15 -0600

    Administrador
    Creacion de la interfaz de administrador

---
+++ administrador.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador</title>
+</head>
+<body>
+<link rel="stylesheet" href="CSS/styles.css">
+<div class="header">
+    <div style="text-align: center;">
+        <h1 class="title">ADMINISTRADOR</h1>
+    </div>
+    <div style="text-align: center;padding-top:50px;background-color: gray;">
+        <a href="toplibros.php"class="hiperVin">Top ventas de libros</a>
+        <a  href="adminclientes.php"class="hiperVin">Administrar Usuarios</a>
+    </div>
+    <div style="text-align: center;padding-top:10px">
+        <img src="IMG/logo2.jpg" style="width:300px;height:300px;padding-right:280px;">
+        <img src="IMG/clientes.jpg"style="width:300px;height:300px;">
+    </div>
+    
+                      
+</div>
+</body>
+</html>
