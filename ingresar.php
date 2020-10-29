2feca01865b3c44893a50e95706e8ff68f437375
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-28 21:38:47 -0600

    Login
    Creacion de la interfaz de login

--- ingresar.php
+++ ingresar.php
@@ -12,9 +12,19 @@
 <div class="header">
     <h1 class="title">INGRESAR</h1>
     <div style="text-align: center;">
-    <img src="IMG/login.png" style="width:100px;height:100px;padding-top:50px;">
+    <img src="IMG/login.png" style="width:150px;height:150px;padding-top:30px;">
     </div>
-    <h4>lol </h4>
+    <div > 
+        <input type="text" name="email" placeholder="Ingresa tu email">
+    </div>
+    <div >
+        <input type="password" name="password" placeholder="Ingresa tu contraseña">
+    </div>
+    <div class="inputS">
+        <input type="button" value="ENVIAR"  style="width:200px;height:30px;padding-top:5xp;">
+    </div>
+  
+   
 </div>
 </body>
 </html>
