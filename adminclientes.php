d1622f2b072450733936e04c5b1db1ab00ad492b
Jesus <jesus.garnica7469@alumnos.udg.mx>
2020-10-28 21:41:51 -0600

    Administracion de clientes
    creacion de administracion de clientes

---
+++ adminclientes.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<link rel="stylesheet" href="CSS/styles.css">
+<div class="header">
+    <div style="text-align: center;">
+        <h1 class="title">ADMINIDTRASION DE CLIENTES</h1>
+    </div>
+    <div style="text-align: center;background-color: gray;">
+            <h1 style="text-aling:center;">Datos de los Clientes</h1>
+     </div>
+     <div style="text-aling: center;">
+        <h3 style="text-aling: center;padding-left:900px">Busqueda</h3>
+        <input type="text" class="inputBusq" name="buesqueda" placeholder="Busca a un Cliente">
+        
+     </div>
+     <div style="text-align: center;">
+        <table> 
+            <tr>
+                    <th>User_id</th>
+                    <th>Correo</th>
+                    <th>Contraseña</th>
+                    <th>Nombre</th>
+                    <th>Sexo  </th>
+                    <th>Compras</th>
+                    </tr>
+                    <tr>
+                    <td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <td><a href="toplibros.php">12</a></td>
+            </tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <td><a href="toplibros.php">12</a></td></tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <td><a href="toplibros.php">12</a></td></tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <td><a href="toplibros.php">12</a></td></tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <td><a href="toplibros.php">12</a></td></tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <td><a href="toplibros.php">12</a></td></tr>
+            <tr><td>Celda 4</td>
+                    <td>Celda 5</td>
+                    <td>Celda 6</td>
+                    <td>Celda 7</td>
+                    <td>M</td>
+                    <td><a href="toplibros.php">12</a></td></tr>
+        </table>
+
+     </div>               
+    
+</body>
+</html>
